3288b9d123d297d98c500d66f6689fcbd2ca2067
Adrian Bercovici <adriansan_93@yahoo.com>
2024-06-27 19:58:41 +0300

    finished commands and socket command dtos

--- src/Dtos/SocketCommands/GetOlderMessagesDto.ts
+++ src/Dtos/SocketCommands/GetOlderMessagesDto.ts
@@ -1,7 +1,7 @@
 import { BaseCommandDto } from "./BaseCommandDto";
-import { GET_OLDER_MESSAGES} from "../../events";
+import { GET_OLDER_MESSAGES_COMMAND} from "../../events";
 export interface GetOlderMessagesDto extends BaseCommandDto{
-        command: typeof GET_OLDER_MESSAGES,
+        command: typeof GET_OLDER_MESSAGES_COMMAND,
         topicId:number,
         startIndex:number,
         count:number,
--- src/Dtos/SocketCommands/PublishCommandDto.ts
+++ src/Dtos/SocketCommands/PublishCommandDto.ts
@@ -1,7 +1,7 @@
 import { BaseCommandDto } from "./BaseCommandDto";
-import {PUBLISH_MESSAGE} from "../../events";
+import {PUBLISH_MESSAGE_COMMAND} from "../../events";
 export interface PublishCommandDto extends BaseCommandDto{
-    command:typeof PUBLISH_MESSAGE;
+    command:typeof PUBLISH_MESSAGE_COMMAND;
     topicId:number;
     content:string;
 }

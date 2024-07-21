5b15565c3f3b08a4f2abaf6a21691ea3c6647c24
Adrian Bercovici <adriansan_93@yahoo.com>
2024-07-21 22:22:57 +0300

    fixed proj

--- src/Config.ts
+++ src/Config.ts
@@ -1,4 +1,6 @@
 const config = {
-    baseHttpUrl: process.env.REACT_APP_API_URL,
-    baseWsUrl: process.env.REACT_APP_WS_URL
-};
+    baseHttpUrl: process.env.REACT_APP_API_URL || 'http://localhost:8080',
+    baseWsUrl: process.env.REACT_APP_WS_URL || 'ws://localhost:8080'
+  };
+  
+  export default config;
